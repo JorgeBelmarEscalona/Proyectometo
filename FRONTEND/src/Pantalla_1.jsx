f3670471a45de8d3d13022b60eb76ca936e23450
jorge <jorgeguillermobelmar@gmail.com>
2023-07-30 09:46:16 -0400

    primera pantalla casi terminada la parte visual

--- FRONTEND/src/Pantalla_1.jsx
+++ FRONTEND/src/Pantalla_1.jsx
@@ -7,10 +7,15 @@ function MiComponente() {
       <div className="Brigadas_de_conce">
         <h1>Brigadas de Concepción</h1>
       </div>
+      <div className="Postulaaqui">
+          ¡Postula Ahora!
+      </div>
 
       <div className="opciones">
         <button className="login">Login</button>
         <button className="preguntasfrecuentes">Preguntas Frecuentes</button>
+        <button className="postula">Postula</button>
+        <img src="bombero.svg" style="float: right; width: 100px; height: 100px;"></img>
       </div>
     </div>
   );
