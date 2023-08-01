2cccbf1dcb30c6c121f603827e1b42f519ccc8c5
jorge <jorgeguillermobelmar@gmail.com>
2023-07-31 23:41:41 -0400

    insercion de cssbaseline

--- FRONTEND/src/main.jsx
+++ FRONTEND/src/main.jsx
@@ -5,10 +5,13 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+
 import './index.css'
+import { CssBaseline } from '@mui/material';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <CssBaseline />
     <Pantalla_1 />
   </React.StrictMode>,
 )
