4a4d621b2d59e42d08ddb6dde7b83dcf93d90e68
jorge <jorgeguillermobelmar@gmail.com>
2023-07-31 23:20:42 -0400

    Importe cosas de mui en main.jsx

--- FRONTEND/src/main.jsx
+++ FRONTEND/src/main.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Pantalla_1 from './Pantalla_1.jsx'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
